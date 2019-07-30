4a006844d909f71aebcf952d211f20e93c783001
Savani <bsavani@paypal.com>
2019-07-30 14:47:40 -0700

    First commit

---
+++ justamonad-tutorials-generics/src/main/java/org/justamonad/tutorials/generics/App.java
@@ -0,0 +1,13 @@
+package org.justamonad.tutorials.generics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ justamonad-tutorials-leetcode/src/main/java/org/justamonad/tutorials/leetcode/App.java
@@ -0,0 +1,13 @@
+package org.justamonad.tutorials.leetcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
