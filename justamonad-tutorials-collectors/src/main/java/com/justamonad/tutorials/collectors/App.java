d766b1e3750cb7a824d87139b688f28fd020d592
Savani <bsavani@paypal.com>
2020-06-23 20:01:55 -0700

    adding collectors module

---
+++ justamonad-tutorials-collectors/src/main/java/com/justamonad/tutorials/collectors/App.java
@@ -0,0 +1,13 @@
+package com.justamonad.tutorials.collectors;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
