58580392ad4ea1364a53db0a1c29ca6c3b6e4b1c
Bharat Savani <savanibharat@gmail.com>
2022-07-06 17:59:01 -0700

    More collectors.

--- justamonad-tutorials-collectors/src/test/java/com/justamonad/tutorials/collectors/GroupingByConcurrentTest.java
+++ justamonad-tutorials-collectors/src/test/java/com/justamonad/tutorials/collectors/GroupingByConcurrentTest.java
@@ -5,43 +5,36 @@ import com.justamonad.tutorials.common.Transactions;
 import com.neovisionaries.i18n.CountryCode;
 import org.junit.Test;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.stream.Collectors;
 
-public class GroupingByTest {
-//https://java-8-tips.readthedocs.io/en/stable/advcollectors.html
-//	Map<String, Optional<Trade>> map1 = trades.stream()   // Solution-1
-//		    .collect(groupingBy(Trade::getRegion, maxBy(comparing(Trade::getNotional))));
-//
-//
-//		Map<String, Trade> map2 = trades.stream()             // Solution-2
-//		        .collect(groupingBy(Trade::getRegion,
-//		                collectingAndThen(maxBy(comparing(Trade::getNotional)), Optional::get)));
+public class GroupingByConcurrentTest {
 
     @Test
-    public void groupingByCountryTest() {
+    public void groupingByConcurrentCountryTest() {
         List<Transaction> dataSet = Transactions.getDataSet();
         dataSet.forEach(val -> System.out.println(val.transactionId() + " :: " + val.country() + " :: " + val.amount()));
 
         Map<CountryCode, List<Transaction>> txnsByCountry = dataSet
                 .stream()
-                .collect(Collectors.groupingBy(Transaction::country));
+                .collect(Collectors.groupingByConcurrent(Transaction::country));
 
         txnsByCountry.forEach((k, v) -> System.out.println(k + "" +
                 v.stream().map(Transaction::transactionId).collect(Collectors.toList())));
     }
 
     @Test
-    public void groupingByCountryAndCountTest() {
+    public void groupingByConcurrentCountryAndCountTest() {
         List<Transaction> dataSet = Transactions.getDataSet();
         dataSet.forEach(val -> System.out.println(val.transactionId() + " :: " + val.country() + " :: " + val.amount()));
 
         Map<CountryCode, Long> totalTxnsByCountry = dataSet
                 .stream()
-                .collect(Collectors.groupingBy(
+                .collect(Collectors.groupingByConcurrent(
                         Transaction::country,
                         Collectors.counting()));
 
@@ -49,13 +42,13 @@ public class GroupingByTest {
     }
 
     @Test
-    public void groupingByCountryAndTotalValuedTransactionsByCountryTest() {
+    public void groupingByConcurrentCountryAndTotalValuedTransactionsByCountryTest() {
         List<Transaction> dataSet = Transactions.getDataSet();
         dataSet.forEach(val -> System.out.println(val.transactionId() + " :: " + val.country() + " :: " + val.amount()));
 
         Map<CountryCode, Double> totalTxnsByCountry = dataSet
                 .stream()
-                .collect(Collectors.groupingBy(
+                .collect(Collectors.groupingByConcurrent(
                         Transaction::country,
                         Collectors.summingDouble(txn -> txn.amount().doubleValue())));
 
@@ -63,30 +56,44 @@ public class GroupingByTest {
     }
 
     @Test
-    public void groupingByCountryAndCountUsingSupplierTest() {
+    public void groupingByConcurrentCountryAndAverageValuedTransactionsByCountryTest() {
+        List<Transaction> dataSet = Transactions.getDataSet();
+        dataSet.forEach(val -> System.out.println(val.transactionId() + " :: " + val.country() + " :: " + val.amount()));
+
+        Map<CountryCode, Double> totalTxnsByCountry = dataSet
+                .stream()
+                .collect(Collectors.groupingByConcurrent(
+                        Transaction::country,
+                        Collectors.averagingDouble(txn -> txn.amount().doubleValue())));
+
+        System.out.println(totalTxnsByCountry);
+    }
+
+    @Test
+    public void groupingByConcurrentCountryAndCountUsingSupplierTest() {
         List<Transaction> dataSet = Transactions.getDataSet();
         dataSet.forEach(val -> System.out.println(val.transactionId() + " :: " + val.country() + " :: " + val.amount()));
 
         Map<CountryCode, Long> totalTxnsByCountry = dataSet
                 .stream()
-                .collect(Collectors.groupingBy(
+                .collect(Collectors.groupingByConcurrent(
                         Transaction::country,
-                        TreeMap::new,
+                        ConcurrentSkipListMap::new,
                         Collectors.counting()));
 
         System.out.println(totalTxnsByCountry);
     }
 
     @Test
-    public void groupingByCountryAndCountUsingSupplierAndMappingTest() {
+    public void groupingByConcurrentCountryAndCountUsingSupplierAndMappingTest() {
         List<Transaction> dataSet = Transactions.getDataSet();
         dataSet.forEach(val -> System.out.println(val.transactionId() + " :: " + val.country() + " :: " + val.amount()));
 
         Map<CountryCode, List<Long>> collect = dataSet
                 .stream()
-                .collect(Collectors.groupingBy(
+                .collect(Collectors.groupingByConcurrent(
                         Transaction::country,
-                        TreeMap::new,
+                        ConcurrentSkipListMap::new,
                         Collectors.mapping(Transaction::transactionId,
                                 Collectors.toList())));
 
--- justamonad-tutorials-object-mapping/src/main/java/com/justamonad/tutorials/object/mapping/main/GetAllBeans.java
+++ justamonad-tutorials-object-mapping/src/main/java/com/justamonad/tutorials/object/mapping/main/GetAllBeans.java
@@ -1,39 +1,37 @@
-package com.justamonad.tutorials.object.mapping.main;
-
-import java.util.Arrays;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ApplicationContext;
-
-import com.justamonad.tutorials.object.mapping.SingletonBean;
-
-@Named
-public class GetAllBeans implements CommandLineRunner {
-
-	@Inject
-	private ApplicationContext applicationContext;
-
-	@Inject
-	@Qualifier("singleton_bean_1")
-	private SingletonBean s1;
-	
-	@Inject
-	@Qualifier("singleton_bean_2")
-	private SingletonBean s2;
-	
-	@Override
-	public void run(String... args) throws Exception {
-		String[] beans = applicationContext.getBeanDefinitionNames();
-		Arrays.sort(beans);
-		for (String bean : beans) {
-			System.out.println(bean);
-		}
-		System.out.println(s1.hashCode());
-		System.out.println(s2.hashCode());
-	}
-
-}
+//package com.justamonad.tutorials.object.mapping.main;
+//
+//import com.justamonad.tutorials.object.mapping.SingletonBean;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.ApplicationContext;
+//
+//import javax.inject.Inject;
+//import javax.inject.Named;
+//import java.util.Arrays;
+//
+//@Named
+//public class GetAllBeans implements CommandLineRunner {
+//
+//    @Inject
+//    private ApplicationContext applicationContext;
+//
+//    @Inject
+//    @Qualifier("singleton_bean_1")
+//    private SingletonBean s1;
+//
+//    @Inject
+//    @Qualifier("singleton_bean_2")
+//    private SingletonBean s2;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        String[] beans = applicationContext.getBeanDefinitionNames();
+//        Arrays.sort(beans);
+//        for (String bean : beans) {
+//            System.out.println(bean);
+//        }
+//        System.out.println(s1.hashCode());
+//        System.out.println(s2.hashCode());
+//    }
+//
+//}
