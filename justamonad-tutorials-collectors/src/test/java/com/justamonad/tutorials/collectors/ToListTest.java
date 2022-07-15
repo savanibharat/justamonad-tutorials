acb37f3eaa88e4055a4cdde2510076e2a52be210
Bharat Savani <savanibharat@gmail.com>
2022-07-14 20:47:35 -0700

    Code fix.

--- justamonad-tutorials-collectors/src/test/java/com/justamonad/tutorials/collectors/ToListTest.java
+++ justamonad-tutorials-collectors/src/test/java/com/justamonad/tutorials/collectors/ToListTest.java
@@ -1,5 +1,6 @@
 package com.justamonad.tutorials.collectors;
 
+import com.google.common.collect.ImmutableList;
 import com.justamonad.tutorials.common.Transaction;
 import com.justamonad.tutorials.common.Transactions;
 import com.neovisionaries.i18n.CountryCode;
@@ -11,13 +12,6 @@ import java.util.stream.Collectors;
 
 public class ToListTest {
 
-//	toList() method is used to collect the data from Stream into the List. As of today the toList() method returns
-//	a Collector that uses ArrayList. That means the data will be collected into an ArrayList.
-//	If you want provide your own implementation of List then you need to use toCollection method. We will come to
-//	that later in this article.
-
-//	One more thing, the result returned is mutable ArrayList. Which means that the returned ArrayList can be modified.
-//	Below example collects the US country's transactions into the List.
     @Test
     public void toList() {
         List<Transaction> dataSet = Transactions.getDataSet();
@@ -35,18 +29,6 @@ public class ToListTest {
         Assert.assertEquals(2, result.size());
     }
 
-//	toUnmodifiableList() method is was added to Collectors class in Java 10.
-//	toUnmodifiableList() method is used to return data in List which cannot be modified. Or the returned List
-//	is immutable.
-//	The result is NOT wrapped in Collections.unmodifiableList(list). A little of history. As of Java 9 List
-//	interface was augmented with several static factory that returns immutable List. [provide article line] Read about it here.
-//	The result is first collected into ArrayList. And the finisher function of Collector is defined as
-//	list -> (List<T>)List.of(list.toArray())
-//	List.of methods copies all the elements from this list into new List and new immutable List is returned.
-
-//	If you are using Java 8 and want to convert to an unmodfiableList then you need to use collectingAndThen method
-//	Collectors.collectingAndThen(Collectors.toList(), Collections::unmodfiableList);
-//	Below example collects the US country's transactions into the immutable List.
     @Test
     public void toUnmodifiableList() {
         List<Transaction> dataSet = Transactions.getDataSet();
@@ -57,6 +39,23 @@ public class ToListTest {
                 .stream()
                 .filter(txn -> txn.country() == CountryCode.US)
                 .collect(Collectors.toUnmodifiableList());
+
+        System.out.println("\nOutput::");
+        result.forEach(val -> System.out.println(val.transactionId() + " :: " + val.country()));
+
+        Assert.assertEquals(2, result.size());
+    }
+
+    @Test
+    public void toGuavaImmutableList() {
+        List<Transaction> dataSet = Transactions.getDataSet();
+        System.out.println("Input::");
+        dataSet.forEach(val -> System.out.println(val.transactionId() + " :: " + val.country()));
+
+        List<Transaction> result = dataSet
+                .stream()
+                .filter(txn -> txn.country() == CountryCode.US)
+                .collect(ImmutableList.toImmutableList());
 
         System.out.println("\nOutput::");
         result.forEach(val -> System.out.println(val.transactionId() + " :: " + val.country()));
--- justamonad-tutorials-collectors/src/test/java/com/justamonad/tutorials/collectors/ToSetTest.java
+++ justamonad-tutorials-collectors/src/test/java/com/justamonad/tutorials/collectors/ToSetTest.java
@@ -1,5 +1,7 @@
 package com.justamonad.tutorials.collectors;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.justamonad.tutorials.common.Transaction;
 import com.justamonad.tutorials.common.Transactions;
 import com.neovisionaries.i18n.CountryCode;
@@ -12,13 +14,6 @@ import java.util.stream.Collectors;
 
 public class ToSetTest {
 
-//	toSet() method is used to collect the data from Stream into the Set. As of today the toSet() method returns
-//	a Collector that uses HashSet. That means the data will be collected into an HashSet.
-//	If you want provide your own implementation of Set then you need to use toCollection method.
-//	Remember Set will filter out duplicate eleemnts.
-
-//	The result returned is mutable HashSet. Which means that the returned HashSet can be modified.
-//	Below example collects the US country's transactions into the Set.
     @Test
     public void toSet() {
 
@@ -38,18 +33,6 @@ public class ToSetTest {
 
     }
 
-//	toUnmodifiableSet() method is was added to Collectors class in Java 10.
-//	toUnmodifiableSet() method is used to return data in Set which cannot be modified. Or the returned Set
-//	is immutable.
-//	The result is NOT wrapped in Collections.unmodifiableSet(set). A little bit of history. As of Java 9 Set
-//	interface was augmented with several static factory that returns immutable Set. [provide article line] Read about it here.
-//	The result is first collected into HashSet. And the finisher function of Collector is defined as
-//	set -> (Set<T>)Set.of(set.toArray())
-//	Set.of methods copies all the elements from this set into new Set and new immutable Set is returned.
-
-    //	If you are using Java 8 and want to convert to an unmodfiableSet then you need to use collectingAndThen method
-//	collect(Collectors.toSet(), Collections::unmodfiableSet);
-//	Below example collects the US country's transactions into the immutable Set.
     @Test
     public void toUnmodifiableSet() {
 
@@ -66,7 +49,23 @@ public class ToSetTest {
         result.forEach(val -> System.out.println(val.transactionId() + " :: " + val.country()));
 
         Assert.assertEquals(2, result.size());
+    }
 
+    @Test
+    public void toGuavaImmutableList() {
+        List<Transaction> dataSet = Transactions.getDataSet();
+        System.out.println("Input::");
+        dataSet.forEach(val -> System.out.println(val.transactionId() + " :: " + val.country()));
+
+        Set<Transaction> result = dataSet
+                .stream()
+                .filter(txn -> txn.country() == CountryCode.US)
+                .collect(ImmutableSet.toImmutableSet());
+
+        System.out.println("\nOutput::");
+        result.forEach(val -> System.out.println(val.transactionId() + " :: " + val.country()));
+
+        Assert.assertEquals(2, result.size());
     }
 
 }
